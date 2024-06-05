03856ecfa305331a65a5f98d95df82dac812ce11
Brian Vazques <vazquesbrian7@gmail.com>
2024-06-04 23:20:45 -0300

    css

--- src/components/ui/MainContent.jsx
+++ src/components/ui/MainContent.jsx
@@ -7,7 +7,7 @@ export const MainContent = () => {
         <h2 className="font-bold p-5 blurbg">PROYECTOS:</h2>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 px-5 mb-5 w-full mx-auto gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 px-5 mb-12 w-full mx-auto gap-12">
         {proyectsData.map((proyect) => (
           <Proyect key={proyect.id} props={proyect} />
         ))}
